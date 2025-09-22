aa729d6ff2ff9691989c70d093aef97443c0a07d
Parth Kathiria <parth.kathiria169@gmail.com>
2025-09-22 17:15:01 -0400

    project set up

---
+++ backend/src/test/java/com/track/pokerchip/poker_backend/PokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track.pokerchip.poker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
